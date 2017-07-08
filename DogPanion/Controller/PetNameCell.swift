f36a77a758b998d2176832f411cdb3fbe3679062
Rick Stoner <stonerrick19@gmail.com>
2017-07-07 22:22:31 -0700

    Added UI elements for main page, updated color scheme, still WIP.

---
+++ DogPanion/Controller/PetNameCell.swift
@@ -0,0 +1,13 @@
+//
+//  PetNameCell.swift
+//  DogPanion
+//
+//  Created by Rick Stoner on 7/7/17.
+//  Copyright © 2017 Rick Stoner. All rights reserved.
+//
+
+import UIKit
+
+class PetNameCell: UICollectionViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+}
