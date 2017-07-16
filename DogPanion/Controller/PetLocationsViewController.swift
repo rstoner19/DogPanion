51251eeeeeaff5f9bc5bef395cce082e462f939e
Rick Stoner <stonerrick19@gmail.com>
2017-07-15 21:50:19 -0700

    Added view controllers of map view used to show various pet locations near user

---
+++ DogPanion/Controller/PetLocationsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PetLocationsViewController.swift
+//  DogPanion
+//
+//  Created by Rick Stoner on 7/15/17.
+//  Copyright © 2017 Rick Stoner. All rights reserved.
+//
+
+import UIKit
+
+class PetLocationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
