e124b13668940476193a0c401d9dd12f9958f020
Rick Stoner <stonerrick19@gmail.com>
2017-07-15 22:05:26 -0700

    commit

---
+++ DogPanion/Controller/PetImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  PetImageCell.swift
+//  DogPanion
+//
+//  Created by Rick Stoner on 7/4/17.
+//  Copyright © 2017 Rick Stoner. All rights reserved.
+//
+
+import UIKit
+
+class PetImageCell: UICollectionViewCell {
+    
+    @IBOutlet weak var petImage: UIImageView!
+    
+}
