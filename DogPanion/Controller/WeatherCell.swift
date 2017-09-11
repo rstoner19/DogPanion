40289af05471af96a349cc0604e5c63c0ccb04ee
Rick Stoner <stonerrick19@gmail.com>
2017-09-10 18:38:15 -0700

    Setup of collection view

---
+++ DogPanion/Controller/WeatherCell.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherCell.swift
+//  DogPanion
+//
+//  Created by Rick Stoner on 9/10/17.
+//  Copyright © 2017 Rick Stoner. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
